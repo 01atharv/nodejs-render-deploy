da7e3fe36bf2189c7253d7dec7789a6f99888585
Atharvraj singh  rana <atharvrajrana1480@gmail.com>
2025-06-24 11:11:14 +0530

    new

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/api/get',(req,res)=>{
+    res.send({message:"hello back to node server"})
+});
+
+
+
+app.listen(8000,()=>{
+  console.log("Server is running on port 8000");
+});
